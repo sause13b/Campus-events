d437f576f668bcde597c06f9f1c71146f7e05182
sause13b <mnx88-2@mail.ru>
2021-06-01 16:10:50 +1000

    Start making registration

--- Campus_events/settings.py
+++ Campus_events/settings.py
@@ -16,6 +16,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'register',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'Campus_events.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +105,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
---
+++ register/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+# Функция регистрации
+def registr(request):
+    data = {}
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            data['form'] = form
+            data['res'] = "Всё прошло успешно"
+            return render(request, 'register/registr.html', data)
+    else:
+        form = UserCreationForm()
+        data['form'] = form
+        # Рендаринг страницы
+        return render(request, 'register/registr.html', data)
