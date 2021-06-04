72a9d11b4f84c62f50746600864d2daa9e6af958
sause13b <mnx88-2@mail.ru>
2021-06-04 10:29:10 +1000

    Исправленны URl и добавлена Welcomepage

--- personalarea/models.py
+++ personalarea/models.py
@@ -2,9 +2,9 @@ from django.db import models
 
 # Create your models here.
 class PersonalAreaInformation(models.Model):
-    login           = models.TextField()
-    date_of_birth   = models.DateField()
-    sex             = models.TextField()
+    login = models.TextField()
+    date_of_birth = models.DateField()
+    sex = models.TextField()
     corporate_email = models.TextField()
-    personal_email  = models.TextField()
-    mobile_phone    = models.TextField()
+    personal_email = models.TextField()
+    mobile_phone = models.TextField()
--- register/views.py
+++ register/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 
-# Функция регистрации
 def registr(request):
     data = {}
     if request.method == 'POST':
