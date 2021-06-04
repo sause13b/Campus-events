29e88ff1f2f2016ad0e70533bbb5020251ed2dab
H05pes <fominyegor.27@gmail.com>
2021-06-04 11:37:57 +1000

    personal area css update

--- personalarea/models.py
+++ personalarea/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 class PersonalAreaInformation(models.Model):
-    login           = models.TextField()
-    date_of_birth   = models.DateField()
-    sex             = models.TextField()
+    sex_choice = (
+        ('М', 'Мужской'),
+        ('Ж', 'Женский'),
+    )
+    login = models.CharField(max_length=20)
+    date_of_birth = models.DateField()
+    sex = models.CharField(max_length=1, choices=sex_choice)
     corporate_email = models.TextField()
-    personal_email  = models.TextField()
-    mobile_phone    = models.TextField()
+    personal_email = models.TextField()
+    mobile_phone = models.TextField()
