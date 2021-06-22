03a22f85004ff425ceecb5435de579923f49738f
Alexander Aglicheev <73017769+f-m-t-t-m@users.noreply.github.com>
2021-06-22 17:42:50 +1000

    Update settings.py

--- Campus_events/settings.py
+++ Campus_events/settings.py
@@ -136,6 +136,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
